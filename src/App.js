df873502803184cf08b7000f5f3e8e1260cc9ca2
Lucaraso <Lucaraso>
2023-07-31 20:34:57 +0100

    Creating expense tracker ui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import './App.css';
+import React from "react";
+import { Header } from "./Components/Header";
+import { Balance } from "./Components/Balance";
+import { IncomeExpenses } from "./Components/IncomeExpenses";
+import { TransactionList } from "./Components/TransactionList";
+import { AddTransaction } from "./Components/AddTransaction";
+import { GlobalProvider } from "./Context/AppReducer";
+
+
+import "./App.css";
 
 function App() {
   return (
-    <div>Hello World</div>
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+      <Balance/>
+      <IncomeExpenses/> 
+      <TransactionList />
+      <AddTransaction />
+      </div>
+    </GlobalProvider>
   );
 }
 
---
+++ src/Components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Header = () => { // Using named export
+  return (
+    <h2>
+      Expense Tracker
+    </h2>
+  );
+}
